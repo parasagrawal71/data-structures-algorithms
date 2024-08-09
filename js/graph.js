ba8773735632ce7cc2db499fff4f787859c4a710
Paras Agrawal <parasagrawal71@gmail.com>
2024-08-10 01:29:22 +0530

    Add graph

---
+++ js/graph.js
@@ -0,0 +1,30 @@
+class Graph {
+  constructor() {
+    this.numOfNodes = 0;
+    this.adjacentList = {};
+  }
+
+  addVertex(node) {
+    if (!this.adjacentList[node]) {
+      this.adjacentList[node] = [];
+      this.numOfNodes++;
+    }
+  }
+
+  addEdge(node1, node2) {
+    // Undirected edges
+    if (this.adjacentList[node1]) {
+      this.adjacentList[node1].push(node2);
+    }
+
+    if (this.adjacentList[node2]) {
+      this.adjacentList[node2].push(node1);
+    }
+  }
+
+  showConnections() {
+    for (const [vertex, connList] of Object.entries(this.adjacentList)) {
+      console.log(`${vertex} --> ${connList.join(", ")}`);
+    }
+  }
+}
