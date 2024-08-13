abbc68524aeaace84a35b28a77ee2fee4fe4df2e
Paras Agrawal <parasagrawal71@gmail.com>
2024-08-14 01:14:13 +0530

    Update graph

--- js/graph.js
+++ js/graph.js
@@ -1,32 +1,32 @@
 class Graph {
   constructor() {
     this.numOfNodes = 0;
-    this.adjacentList = {};
+    this.adj = {}; // adj is adjacency list
   }
 
   addVertex(node) {
-    if (!this.adjacentList[node]) {
-      this.adjacentList[node] = [];
+    if (!this.adj[node]) {
+      this.adj[node] = [];
       this.numOfNodes++;
     }
   }
 
   addEdge(node1, node2) {
-    if (this.adjacentList[node1]) {
-      this.adjacentList[node1].push(node2);
+    if (this.adj[node1]) {
+      this.adj[node1].push(node2);
     }
 
-    // if (this.adjacentList[node1] && !this.adjacentList[node1].includes(node2)) {
-    //   this.adjacentList[node1].push(node2);
+    // if (this.adj[node1] && !this.adj[node1].includes(node2)) {
+    //   this.adj[node1].push(node2);
     // }
 
-    // if (this.adjacentList[node2] && !this.adjacentList[node2].includes(node1)) {
-    //   this.adjacentList[node2].push(node1);
+    // if (this.adj[node2] && !this.adj[node2].includes(node1)) {
+    //   this.adj[node2].push(node1);
     // }
   }
 
   showConnections() {
-    for (const [vertex, connList] of Object.entries(this.adjacentList)) {
+    for (const [vertex, connList] of Object.entries(this.adj)) {
       console.log(`${vertex} --> ${connList.join(", ")}`);
     }
   }
